d459b6ddf3789127656304941efffb0418b2d0b9
Shakiba <shakiba2013@gmail.com>
2024-10-16 12:08:34 +0330

    added ground

--- ground.js
+++ ground.js
@@ -1,7 +1,15 @@
-import { incrementCustomProperty } from "./updateCustomProperty.js;";
+import {
+  incrementCustomProperty,
+  setCustomProperty,
+} from "./updateCustomProperty.js;";
 
 const SPEED = 0.05;
 const groundElems = document.querySelectorAll("[data-ground]");
+
+export function setupGround() {
+  setCustomProperty(groundElems[0], "--left", 0);
+  setCustomProperty(groundElems[1], "--left", 300);
+}
 
 export function updateGround(delta) {
   groundElems.forEach((ground) => {
