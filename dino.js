d409adc08a78febf7f6535ac0d44552fa6bd5732
Shakiba <shakiba2013@gmail.com>
2024-10-18 10:34:07 +0330

    added dino

---
+++ dino.js
@@ -0,0 +1,9 @@
+const dinoElem = document.querySelector("[data-dino]");
+const JUMP_SPEED = 0.45;
+const GRAVITY = 0.0015;
+const DINO_FRAME_COUNT = 2;
+const FRAME_TIME = 100;
+
+export function setupDino() {}
+
+export function updateDino(delta, speedScale) {}
