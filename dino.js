94766b99e076df6e38f75a13952698191db794e6
Shakiba <shakiba2013@gmail.com>
2024-10-18 10:46:19 +0330

    added dino

--- dino.js
+++ dino.js
@@ -5,19 +5,31 @@ const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100;
 
 let isJumping;
+let dinoFrame;
+let currentFrameTime;
+
 export function setupDino() {}
+
 isJumping = false;
+dinoFrame = 0;
+currentFrameTime = 0;
 
 export function updateDino(delta, speedScale) {
-  handleRun();
+  handleRun(delta, speedScale);
   handleJump();
 }
 
-function handleRun() {
+function handleRun(delta, speedScale) {
   if (isJumping) {
     dinoElem.src = `imgs/dino-stationary.png`;
     return;
   }
+  if (currentFrameTime >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT;
+    dinoElem.src = `imgs/dino-run-${dinoFrame}.png`;
+    currentFrameTime -= FRAME_TIME;
+  }
+  currentFrameTime += delta * speedScale;
 }
 
 function handleJump() {}
